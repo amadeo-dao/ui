ee0e04dcfdc049288b0146db9b66160938933f2e
Ned Albo <ned.albo@mailfence.com>
2023-01-13 18:52:52 +0100

    disabled ManagerSection if user is not manager

--- components/ManagerSection.tsx
+++ components/ManagerSection.tsx
@@ -1,9 +1,24 @@
 import { Grid } from '@mui/material';
+import { useContext } from 'react';
+import { useAccount, useContractRead } from 'wagmi';
+import VaultContext, { vaultABI } from '../lib/hooks/useVault';
 import ReturnFunds from './ManagerSection/ReturnFunds';
 import ShareholderManagement from './ManagerSection/ShareholderManagement';
 import WithdrawFunds from './ManagerSection/WithdrawFunds';
 
 export default function ManagerSection() {
+  const vault = useContext(VaultContext);
+
+  const { address: account } = useAccount();
+
+  const { data: manager } = useContractRead({
+    address: vault.address,
+    abi: vaultABI,
+    functionName: 'manager',
+    enabled: !!account
+  });
+
+  if (!manager || !account || account !== manager) return <></>;
   return (
     <Grid container mt={'4em'}>
       <Grid item xs={2}></Grid>
