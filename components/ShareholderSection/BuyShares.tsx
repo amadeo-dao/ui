b86d66d764f235da95e5c037dc48368b92a5571a
Ned Albo <ned.albo@mailfence.com>
2023-01-19 15:17:47 +0100

    created MintForm

--- components/ShareholderSection/BuyShares.tsx
+++ components/ShareholderSection/BuyShares.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Section from '../displays/Section';
 import DepositForm from './DepositForm';
+import MintForm from './MintForm';
 
 function BuyShares() {
   const [isDepositMode, setDepositMode] = useState<boolean>(true);
@@ -11,6 +12,7 @@ function BuyShares() {
   return (
     <Section heading="Buy Shares" headingAlign="center">
       {isDepositMode && <DepositForm onSwitchMode={handleSwitchMode}></DepositForm>}
+      {!isDepositMode && <MintForm onSwitchMode={handleSwitchMode}></MintForm>}
     </Section>
   );
 }
